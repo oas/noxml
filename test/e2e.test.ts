469e137a6dcf25b1b4e4e38c9115a13b767d2a44
Jonas Tamimi <oas@users.noreply.github.com>
2024-05-22 13:32:59 +0200

    Added more e2e tests

--- test/e2e.test.ts
+++ test/e2e.test.ts
@@ -5,8 +5,8 @@ import {Builder} from "../src/builder";
 const parser = new Parser();
 const builder = new Builder();
 
-describe('simple tests', () => {
-	it('convert simple xml', async () => {
+describe('end to end tests', () => {
+	it('convert simple example', async () => {
 		const expected =
 			'<?xml version="1.0" encoding="UTF-8" standalone="yes"?>\n' +
 			'<xml>\n' +
@@ -19,7 +19,7 @@ describe('simple tests', () => {
 		assert.strictEqual(actual, expected);
 	});
 
-	it('convert simple xml with attributes', async () => {
+	it('convert simple example with attributes', async () => {
 		const expected =
 			'<?xml version="1.0" encoding="UTF-8" standalone="yes"?>\n' +
 			'<xml>\n' +
@@ -32,7 +32,7 @@ describe('simple tests', () => {
 		assert.strictEqual(actual, expected);
 	});
 
-	it('convert simple xml with escaped characters', async () => {
+	it('convert simple example with escaped characters', async () => {
 		const expected =
 			'<?xml version="1.0" encoding="UTF-8" standalone="yes"?>\n' +
 			'<xml>\n' +
@@ -45,7 +45,7 @@ describe('simple tests', () => {
 	});
 
 	/*
-	it('convert simple xml with cdata', async () => {
+	it('convert simple example with cdata', async () => {
 		const expected =
 			'<?xml version="1.0" encoding="UTF-8" standalone="yes"?>\n' +
 			'<xml>\n' +
@@ -59,4 +59,39 @@ describe('simple tests', () => {
 		assert.strictEqual(actual, expected);
 	});
 	 */
+
+	it('convert simple example while keeping the order', async () => {
+		const expected =
+			'<?xml version="1.0" encoding="UTF-8" standalone="yes"?>\n' +
+			'<xml>\n' +
+			'\t<foo>1</foo>\n' +
+			'\t<bar>2</bar>\n' +
+			'\t<foo bool="true">3</foo>\n' +
+			'</xml>';
+		let json = await parser.parse(expected);
+		const actual = builder.build(json);
+
+		assert.strictEqual(actual, expected);
+	});
+
+	it('convert nested example while keeping the order', async () => {
+		const expected =
+			'<?xml version="1.0" encoding="UTF-8" standalone="yes"?>\n' +
+			'<xml>\n' +
+			'\t<foo>1</foo>\n' +
+			'\t<foo bool="true">2</foo>\n' +
+			'\t<bar>3</bar>\n' +
+			'\t<baz number="2">\n' +
+			'\t\t<child1>1</child1>\n' +
+			'\t\t<child2>2</child2>\n' +
+			'\t\t<child1>3</child1>\n' +
+			'\t</baz>\n' +
+			'</xml>';
+		let json = await parser.parse(expected);
+		console.info(JSON.stringify(json, null, 2));
+
+		const actual = builder.build(json);
+
+		assert.strictEqual(actual, expected);
+	});
 });
